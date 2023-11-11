b22fd48e1590640516264e107c133ac6c4658e3e
sunepoulsen <sunepoulsen@gmail.com>
2023-11-11 23:37:56 +0100

    TES-107: Extend pipeline step to publish to local maven repository as well.

--- pipeline-publish.sh
+++ pipeline-publish.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-echo "Publish library"
+echo "Publish library to local Maven repository"
+./gradlew publishToMavenLocal
+
+echo "Publish library to remote repository"
 ./gradlew publish
